da6c78c1006e203e659f73b37e791c7bf940abc9
Suvarna <rajesh.suvarna@capgemini.com>
2023-04-20 15:56:06 +0530

    first commit

--- provider.tf
+++ provider.tf
@@ -15,7 +15,7 @@ project = var.project_id
 region = var.region
 zone = "us-central1-a"
 #credentials = "C:\\Users\\RASUVARN\\Downloads\\terraform-learn-382907-f703fb75e1d6.json"
-#comment gfwe4tfu
+#comment gfwe4tfufguygf
 
 
 }
